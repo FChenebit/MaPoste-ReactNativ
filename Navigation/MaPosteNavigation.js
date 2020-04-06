bb525e2787c30d6b16e054526ce84d94421bfb00
Francois Chenebit <francois.chenebit@laposte.net>
2020-04-06 11:58:55 +0200

    Finalization of the drawer navigator
    Correcting the drawer navigator to display icons, and label in white.

--- Navigation/MaPosteNavigation.js
+++ Navigation/MaPosteNavigation.js
@@ -1,16 +1,13 @@
 import * as React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import {createDrawerNavigator,  DrawerContentScrollView, DrawerItemList,DrawerItem} from '@react-navigation/drawer';
-import { ImageBackground } from "react-native";
+import { ImageBackground,Image,Text } from "react-native";
 
 import SearchPost from '../Components/SearchPost'
 import ListPost from '../Components/ListPost'
 import CheckAddress from '../Components/CheckAddress'
 import DetailAddress from '../Components/DetailAddress'
 import StampPrice from '../Components/StampPrice'
-
-import iconPostOffice from '../assets/iconPostOffice.png'
-
 
 const SearchStackNavigator = createStackNavigator();
 
@@ -64,11 +61,6 @@ function CustomDrawerContent(props) {
     <ImageBackground source={require('../assets/drawerBackground.png')} style={{ width:'100%', height:'100%'}}>
     <DrawerContentScrollView {...props}>
         <DrawerItemList {...props} />
-        <DrawerItem
-          label="Search"
-          icon={{iconPostOffice}}
-          onPress={() => props.navigation.navigate('Search')}      
-        />
     </DrawerContentScrollView>
     </ImageBackground>
   );
@@ -83,14 +75,26 @@ function AppDrawer() {
       <AppDrawerNavigator.Screen 
         name='Search' 
         component={SearchStack}
+        options={{ 
+          drawerLabel:() => {return <Text style={{color:'white'}}>Search</Text>},
+          drawerIcon:() => {return <Image source={require('../assets/iconPostOffice.png')} style={{ width:40, height:40}}/>}
+        }}
       />
       <AppDrawerNavigator.Screen 
         name='Address' 
         component={AddressStack}
+        options={{ 
+          drawerLabel:() => {return <Text style={{color:'white'}}>Address</Text>},
+          drawerIcon:() => {return <Image source={require('../assets/iconAddress.png')} style={{ width:40, height:40}}/>}
+        }}
       />
       <AppDrawerNavigator.Screen 
         name='Stamp' 
         component={StampStack}
+        options={{ 
+          drawerLabel:() => {return <Text style={{color:'white'}}>Stamp</Text>},
+          drawerIcon:() => {return <Image source={require('../assets/iconStamp.png')} style={{ width:40, height:40}}/>}
+        }}
       />
     </AppDrawerNavigator.Navigator>
   )
