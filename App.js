6bdc049b54f3ba7047b3b6fa0ae4d148910dd1e1
Francois Chenebit <francois.chenebit@laposte.net>
2020-04-10 16:51:21 +0200

    SafeArea
    Managing a safe area AND an Image Background

--- App.js
+++ App.js
@@ -1,27 +1,17 @@
-/*import React from 'react';
-import {NavigationContainer } from '@react-navigation/native'
-
-
-import MaPosteNavigation from './Navigation/MaPosteNavigation'
-
-export default function App() {
-  return (
-    <NavigationContainer>
-      <MaPosteNavigation/>
-    </NavigationContainer>
-  );
-}
-*/
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
-
-import MyStack from './Navigation/MaPosteNavigation'
+import MyNavigation from './Navigation/MaPosteNavigation'
 
 export default function App() {
+
+
   return (
-    <NavigationContainer>
-      <MyStack />
-    </NavigationContainer>
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <MyNavigation />
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 }
--- Components/StampPrice.js
+++ Components/StampPrice.js
@@ -1,14 +1,19 @@
 import React from 'react'
-import { StyleSheet, View, Text} from 'react-native'
-
+import { StyleSheet, View, Text, ImageBackground } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 class StampPrice extends React.Component {
 
   render() {
+
     return (
-      <View style={styles.container}>
-        <Text>StampPrice</Text>
-      </View>
+      <ImageBackground style={styles.backgroundImage} source={require('../assets/background.jpg')} >
+        <SafeAreaView>
+          <View style={styles.container}>
+            <Text style={styles.title}>Stamp Prices </Text>
+          </View>
+        </SafeAreaView>
+      </ImageBackground>
     )
   }
 
@@ -17,9 +22,27 @@ class StampPrice extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  backgroundImage: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  title: {
+    fontWeight: 'bold',
+    fontSize: 35,
+    flex: 1,
+    flexWrap: 'wrap',
+    marginLeft: 5,
+    marginRight: 5,
+    marginTop: 10,
+    marginBottom: 10,
+    color: '#000000',
+    textAlign: 'center',
   },
 });
 
